26c880d447e403c9ee51885e435f50c914072957
Raider-hub <timedoula@gmail.com>
2022-11-25 15:44:20 +0100

    feat: :fire: Gradient Bckground has been added together with adaptive scrollbar

--- buy_nft/lib/responsive/screens/desktop_screen.dart
+++ buy_nft/lib/responsive/screens/desktop_screen.dart
@@ -1,15 +1,47 @@
+import 'package:adaptive_scrollbar/adaptive_scrollbar.dart';
+import 'package:buy_nft/gen/colors.gen.dart';
+import 'package:buy_nft/widgets/gradients.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-
+import 'package:nb_utils/nb_utils.dart';
 
 class DesktopScreen extends HookWidget {
   const DesktopScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.red,
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    final ScrollController verticalScroll = ScrollController();
+
+    return Scaffold(
+      backgroundColor: AppColor.primary,
+      body: SizedBox(
+        height: height,
+        width: width,
+        child: AdaptiveScrollbar(
+          
+          controller: verticalScroll,
+          child: ListView(
+            controller: verticalScroll,
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  gradientTop,
+                  gradientRight,
+                  Column(
+                    children: const [
+                      
+                 
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
