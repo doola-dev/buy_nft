4193e9e2c08b4c1616f67a623a0d9525d9ec6f0b
Raider-hub <timdoula@gmail.com>
2023-08-02 12:41:02 +0100

    docs(Mobile View): :art: Added Background color for items on navigation tab

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -1,3 +1,4 @@
+import 'package:buy_nft/gen/colors.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nb_utils/nb_utils.dart';
@@ -6,21 +7,24 @@ var navigationBar = Row(
   children: [
     ElevatedButton(
         onPressed: () {},
-        
         child: Text('about',
             style: GoogleFonts.outfit(color: Colors.white, fontSize: 17))),
     75.width,
-   ElevatedButton( onPressed:(){}, 
+    ElevatedButton(
+      onPressed: () {},
       child: Text('pricing',
           style: GoogleFonts.outfit(color: Colors.white, fontSize: 17)),
     ),
     75.width,
-   ElevatedButton( onPressed:(){}, 
+    ElevatedButton(
+      onPressed: () {},
       child: Text('contacts',
           style: GoogleFonts.outfit(color: Colors.white, fontSize: 17)),
     ),
     75.width,
-   ElevatedButton( onPressed:(){}, child: Text('buy nft',
+    ElevatedButton(
+      onPressed: () {},
+      child: Text('buy nft',
           style: GoogleFonts.outfit(color: Colors.white, fontSize: 17)),
     ),
   ],
@@ -28,17 +32,34 @@ var navigationBar = Row(
 var verticalNavigationBar = Column(
   crossAxisAlignment: CrossAxisAlignment.start,
   children: [
-    ElevatedButton( onPressed:(){}, child: Text('About', style: GoogleFonts.outfit(color: Colors.white, fontSize: 20))),
+    ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: AppColor.primary.withOpacity(0.7)),
+        onPressed: () {},
+        child: Text('About',
+            style: GoogleFonts.outfit(color: Colors.white, fontSize: 20))),
     75.height,
-   ElevatedButton( onPressed:(){}, child: Text('Pricing',
+    ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          backgroundColor: AppColor.primary.withOpacity(0.7)),
+      onPressed: () {},
+      child: Text('Pricing',
           style: GoogleFonts.outfit(color: Colors.white, fontSize: 20)),
     ),
     75.height,
-   ElevatedButton( onPressed:(){}, child: Text('Contacts',
+    ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          backgroundColor: AppColor.primary.withOpacity(0.7)),
+      onPressed: () {},
+      child: Text('Contacts',
           style: GoogleFonts.outfit(color: Colors.white, fontSize: 20)),
     ),
     75.height,
-   ElevatedButton( onPressed:(){}, child: Text('Buy nft',
+    ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          backgroundColor: AppColor.primary.withOpacity(0.7)),
+      onPressed: () {},
+      child: Text('Buy nft',
           style: GoogleFonts.outfit(color: Colors.white, fontSize: 20)),
     ),
   ],
